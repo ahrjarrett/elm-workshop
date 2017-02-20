c57cda4780854dffd348bfda922bbcb31845fdcb
Andrew Jarrett <ahrjarrett@gmail.com>
2017-02-19 22:06:46 -0700

    initial commit

---
+++ part1/Main.elm
@@ -0,0 +1,16 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+main =
+    div [ class "content" ]
+        [ header []
+            [
+              h1 [] [ text "ElmHub" ]
+            , span
+                [ class "tagline" ]
+                [ text "Like Github, but for Elm things." ]
+            ]
+        ]
